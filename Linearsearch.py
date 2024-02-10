05b8a8f0654847bee4e4139f7a91f083a3ac2690
Pranshul Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2024-02-11 02:22:33 +0530

    Update Linearsearch.py

--- Linearsearch.py
+++ Linearsearch.py
@@ -1,9 +1,18 @@
 def Linear(arr, N, x):
+    
+    #Iterate through the entire array
     for i in range(0, N):
+        
+        #check if current element is equal to the key element
         if arr[i] == x:
+            
+            #Return the index if element is found
             return i
+        
+    #If element is not found then return -1    
     return -1
 
+#Print the list
 arr = [10,50,30,40,20]
 x = 40
 N = len(arr)
